24e6eeb55e1d2866541540a3ae34e05a68759a1e
AmirNikravan <90094668+AmirNikravan@users.noreply.github.com>
2021-12-27 20:44:45 +0330

    Add files via upload
    it's the first one

---
+++ 3406.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int fib(int n);
+int main(int argc, char** argv) {
+ int n, r,k;
+scanf("%d",&n);
+if(n<1)
+    printf("Error: n is not positive");
+else if(n>0){
+   printf("0 ");
+        for(int i=1;i<n;i++)
+        {
+               r=fib(i);
+printf("%d ",r);
+        }
+
+}
+return 0;
+}
+int fib(int n)
+{
+    int temp=1;
+    if(n>2)
+        temp=fib(n-1)+fib(n-2);
+return temp;
+if(n=1)
+    temp=0;
+    return temp;
+}
