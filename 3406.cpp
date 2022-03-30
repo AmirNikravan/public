2ff9946fdda7186f1b55b32de3beef6fb97bdcf1
AmirHossein Nikravan <amirnikravan@outlook.com>
2022-03-30 17:27:32 +0430

    i change scanf to std::cin and printf to std::cout

--- 3406.cpp
+++ 3406.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* this is just a test*/
 int fib(int n);
-int main(int argc, char** argv) {
- int n, r,k;
-scanf("%d",&n);
-if(n<1)
-    printf("Error: n is not positive");
-else if(n>0){
-   printf("0 ");
-        for(int i=1;i<n;i++)
+int main(int argc, char **argv)
+{
+    int n, r, k;
+    std::cin >> n;
+    if (n < 1)
+        std::cout << ("Error: n is not positive");
+    else if (n > 0)
+    {
+        std::cout << ("0 ");
+        for (int i = 1; i < n; i++)
         {
-               r=fib(i);
-printf("%d ",r);
+            r = fib(i);
+            std::cout << ("%d ", r);
         }
-
-}
-return 0;
+    }
+    return 0;
 }
 int fib(int n)
 {
-    int temp=1;
- 
-    if(n>2)
-        temp=fib(n-1)+fib(n-2);
-return temp;
-if(n=1)
-    temp=0;
+    int temp = 1;
+
+    if (n > 2)
+        temp = fib(n - 1) + fib(n - 2);
+    return temp;
+    if (n = 1)
+        temp = 0;
     return temp;
 }
